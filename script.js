327db9b32ddc296d1e5f80921de512c49fd81f60
rootcat <r00tca7@gmail.com>
2024-11-01 09:27:41 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === 'root' && password === 'rootcat') {
+        window.location.href = '목록/index.html';
+    } else {
+        document.getElementById('message').textContent = '아이디 또는 비밀번호가 잘못되었습니다.';
+    }
+});
