e4b6bb9ddb13dab092803726de5e610355cd46db
rootcat <r00tca7@gmail.com>
2024-11-22 19:40:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
+
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    login(username, password);
-});
-
-function login(username, password) {
     if (username === 'root' && password === 'rootcat') {
         window.location.href = 'indexpage/index.html';
     } else {
-        document.getElementById('message').textContent = '아이디 또는 비밀번호가 잘못되었습니다.';
-        console.log('아이디 또는 비밀번호가 잘못되었습니다.');\
+        document.getElementById('message').textContent = '아이디 또는 비밀번호가 올바르지 않습니다.';
     }
-}
-\
-console.log('로그인 하려면 "login(username, password)"를 호출하세요.');
+});
